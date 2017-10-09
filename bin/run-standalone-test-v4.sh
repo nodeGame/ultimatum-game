e5d48742ac7b7871b58aa4ad67fb1a66d11b8e54
Stefano Balietti <futur.dorko@gmail.com>
2017-10-08 20:08:37 -0400

    mods

---
+++ bin/run-standalone-test-v4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Ultimatum automatic test run
+# Copyright(c) 2015 Stefano Balietti
+# MIT Licensed
+#
+# Run this from inside the ultimatum directory inside nodegame/games/:
+#  $ bin/run-standalone-test.sh
+#
+# Used for testing this package under other packages.
+# Returns true if and only if the tests are run successfully.
+
+# Return on failure immediately.
+set -e
+
+# Go to the nodegame directory and run the automatic game.
+cd ../..
+node test/launcher-autoplay.js ultimatum
+cd games/ultimatum
+
+npm test
