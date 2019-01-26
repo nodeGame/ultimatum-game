acc29c012a25b21813730442eb0a6578a3e386a6
Stefano Balietti <stbaliet@microsoft.com>
2019-01-25 21:14:19 -0500

    remember to reset game.settings and requirements.settings

--- requirements/requirements.settings.js
+++ requirements/requirements.settings.js
@@ -24,19 +24,19 @@ module.exports = {
      * ## maxExecTime
      *
      * If set, limits the maximum execution time for all requirement tests
-     */ 
+     */
     maxExecTime: 6000,
 
     /**
      * ## speedTest
      *
      * If set, client must exchange messages with server "quickly enough"
-     */ 
-    // speedTest: {
-    //     messages: 10,
-    //     time: 1000
-    // },
-    
+     */
+    speedTest: {
+         messages: 10,
+         time: 1000
+    },
+
     /**
      * ## cookieSupport
      *
@@ -47,20 +47,20 @@ module.exports = {
      *   - 'persistent': cookies must persist across session
      *   - 'session': cookies must be set within same session
      */
-    cookieSupport: 'persistent'
+    cookieSupport: 'persistent',
 
     /**
      * ## viewportSize
      *
      * If set, client must have a resolution between the min and max specified
      */
-    // viewportSize: {
-    //     minX: 1366,
-    //     minY: 768,
-    //     // maxX: 13660,
-    //     // maxY: 7680
-    // },
-  
+    viewportSize: {
+         minX: 800,
+         minY: 600,
+         // maxX: 13660,
+         // maxY: 7680
+    },
+
    /**
      * ## browserDetect
      *
@@ -76,23 +76,23 @@ module.exports = {
      *
      * @see https://github.com/faisalman/ua-parser-js
      */
-    // browserDetect: {
-    //     cb: function(ua, params) {
-    //         if (ua.device.model || ua.device.type) {
-    //             return {
-    //                 success: false,
-    //                 errors: [ 'It seems you are using a mobile or tablet ' +
-    //                           'device. You can participate to this game ' +
-    //                           'only from a desktop or laptop computer ' +
-    //                           'with a keyboard and a mouse. If you ' +
-    //                           'can, try with another browser or device.' ]
-    //             };
-    //         }
-    // 
-    //         return { success: true };
-    //     }
-    // },
-    
+    browserDetect: {
+      cb: function(ua, params) {
+          if (ua.device.model || ua.device.type) {
+              return {
+                  success: false,
+                  errors: [ 'It seems you are using a mobile or tablet ' +
+                            'device. You can participate to this game ' +
+                            'only from a desktop or laptop computer ' +
+                            'with a keyboard and a mouse. If you ' +
+                            'can, try with another browser or device.' ]
+              };
+          }
+
+          return { success: true };
+      }
+  },
+
     /**
      * ## nextRoom
      *
@@ -107,7 +107,7 @@ module.exports = {
      *
      * If TRUE, start testing the requirements immediately. Default, TRUE
      */
-    // doChecking: true,
+    doChecking: true,
 
     /**
      * ## logicPath
@@ -119,7 +119,7 @@ module.exports = {
     // # Reserved words for future requirements settings.
 
     //  mode: 'auto',
-    // 
+    //
     //  page: 'requirements.htm'
 
 };
