06353c43e507eb6224ddb4a731094b6bbb44b0de
Stefano Balietti <futur.dorko@gmail.com>
2020-01-29 23:53:35 +0100

    refactored autoplay

--- game/client_types/autoplay.js
+++ game/client_types/autoplay.js
@@ -1,81 +1,57 @@
 /**
- * # Autoplay code for Ultimatum Game
- * Copyright(c) 2017 Stefano Balietti
- * MIT Licensed
- *
- * Handles automatic play.
- *
- * http://www.nodegame.org
- */
+* # Autoplay code for Ultimatum Game
+* Copyright(c) 2020 Stefano Balietti
+* MIT Licensed
+*
+* Handles automatic play.
+*
+* http://www.nodegame.org
+*/
+
+const ngc =  require('nodegame-client');
 
 module.exports = function(treatmentName, settings, stager, setup, gameRoom) {
 
-    var ngc =  require('nodegame-client');
-
-    var game, stager;
-
-    game = gameRoom.getClientType('player');
+    // Retrieve the player client type and rename its nodename property.
+    let game = gameRoom.getClientType('player');
     game.nodename = 'autoplay';
-
+    // Create a new stager based on the player client type.
     stager = ngc.getStager(game.plot);
 
-    stager.extendAllSteps(function(o) {
-        var role;
-        if (o.roles) {
-            o._roles = {};
-            for (role in o.roles) {
-                if (o.roles.hasOwnProperty(role)) {
-                    // Copy only cb property.
-                    o._roles[role] = o.roles[role].cb;
-                    // Make a new one.
-                    o.roles[role].cb = function() {
-                        var _cb, stepObj, id;
-                        stepObj = this.getCurrentStepObj();
-                        id = stepObj.id
-
-                        _cb = stepObj._roles[this.role];
-                        _cb.call(this);
-
-                        if ((this.role === 'BIDDER' && id === 'bidder') ||
-                            (this.role === 'RESPONDENT' &&
-                             id === 'respondent')) {
-
-                            node.on('PLAYING', function() {
-                                node.timer.randomExec(function() {
-                                    node.game.timer.doTimeUp();
-                                });
-                            });
-                        }
-                    }
-                }
-            }
-        }
-        else {
-            o._cb = o.cb;
-            o.cb = function() {
-                var _cb, stepObj, id;
-                stepObj = this.getCurrentStepObj();
-                id = stepObj.id
-
-                _cb = stepObj._cb;
-                _cb.call(this);
-                
+    // Modyfy the new stager's init property, so that at every step
+    // it performs an automatic choice, after the PLAYING even is fired.
+    let origInit = stager.getOnInit();
+    if (origInit) stager.setDefaultProperty('origInit', origInit);
+    stager.setOnInit(function() {
+        let origInit = node.game.getProperty('origInit');
+        if (origInit) origInit.call(this);
+        node.on('PLAYING', function() {
+            let id = node.game.getCurrentStepObj().id;
+            setTimeout(function() {
+                // Widget steps.
                 if (id === 'quiz' ||
-                    id === 'questionnaire' || 
+                    id === 'questionnaire' ||
                     id === 'mood') {
-                    
-                    node.on('PLAYING', function() {
-                        node.timer.randomExec(function() {
-                            node.game.timer.doTimeUp();
+
+                        node.widgets.lastAppended.setValues({
+                            correct: true
                         });
+                }
+
+                if ((node.game.role === 'BIDDER' && id === 'bidder') ||
+                        (node.game.role === 'RESPONDENT' &&
+                        id === 'respondent')) {
+
+                    node.timer.randomExec(function() {
+                        node.game.timer.doTimeUp();
                     });
                 }
                 else if (id !== 'precache' && id !== 'endgame') {
                     node.timer.randomDone(2000);
                 }
-            };
-        }
-        return o;
+
+            }, 2000);
+        });
     });
 
     game.plot = stager.getState();
